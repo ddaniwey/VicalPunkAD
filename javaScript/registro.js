7a8f11b4efbff66a5993ad30cb1f8a6ebbda969c
ddaniwey <ddaniwey@gmail.com>
2024-02-14 18:43:14 +0100

    Añadido

---
+++ javaScript/registro.js
@@ -0,0 +1,8 @@
+// Solicitar al usuario que acepte las cookies
+if (!confirm("Acepta las cookies\nporfi plis <3")) {
+    // Si no confirma, mostrar un mensaje y mantener en un bucle
+
+    while (!confirm("Acepta las cookies\nPorfi plis <3"));
+} else {
+    alert("Gracias Punky");
+}
