34ff331c03d7f8f50c70504915e8450473e035f3
artemisml <156460749+artemisml@users.noreply.github.com>
2024-02-18 16:48:28 +0100

    Add files via upload
    He añadido un Alert de bienvenida

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+alert ("Bienvenida a nuestra asociación\nHemos hecho un calendario con las principales actividades que ofrecemos. Apúnta todo lo que quieras en el calendario de abajo!")
 
 
 function cosa(dia) {
     document.getElementById(dia).innerHTML = prompt("Eventos");
 }
+
